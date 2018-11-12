6d72cde213c3cdea997a5e07539ddd02bafc01eb
Alessandra Boccuto <aless97b@gmail.com>
2018-11-12 15:58:08 +0100

    created prescription class, prescriptions component, prescription service

---
+++ src/app/components/prescriptions/prescriptions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prescriptions',
+  templateUrl: './prescriptions.component.html',
+  styleUrls: ['./prescriptions.component.css']
+})
+export class PrescriptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
