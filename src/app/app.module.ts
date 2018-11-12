693d413a441acc5eba8980f4c0487facf907fc67
Michele <michele.celozzi@outlook.it>
2018-11-12 20:38:15 +0100

    Update Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewListComponent } from './components/view-list/view-list.component';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewListComponent,
+    PatientListComponent
   ],
   imports: [
     BrowserModule,
