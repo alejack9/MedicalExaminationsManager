9ec55e1128b9feff69686b03d1458201bf4b3173
Michele <michele.celozzi@outlook.it>
2018-11-13 17:54:59 +0100

    Update ViewPatientList

--- src/app/components/patient-list.ts
+++ src/app/components/patient-list.ts
@@ -1,8 +1,8 @@
 import { Patient } from 'src/app/classes/Patient';
 
 export const PATIENTS: Patient[] = [
-    {id: 1, name: 'Alessandro', surname: 'Boccuto', examType: 'Neurologia', date: "2018-11-13"},
-    {id: 2, name: 'Manuel', surname: 'Giacchè', examType: 'Ginecologia', date: "2018-11-19"},
+    {id: 1, name: 'Alessandro', surname: 'Giacché', examType: 'Chirurgia', date: "2018-11-13"},
+    {id: 2, name: 'Manuel', surname: 'Cretone', examType: 'Dentista', date: "2018-11-19"},
     {id: 3, name: 'Michele', surname: 'Celozzi', examType: 'Ortopedia', date: "2018-11-13"},
-    {id: 4, name: 'Alessandra', surname: 'Cretone', examType: 'Neurologia', date: "2018-11-22"}
+    {id: 4, name: 'Alessandra', surname: 'Boccuto', examType: 'Oculista', date: "2018-11-22"}
 ];
