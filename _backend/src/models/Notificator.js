f1ad54868271adb55c4c918cdc70347914f7460b
Michele <michele.celozzi@outlook.it>
2019-01-04 13:00:50 +0100

    Update

--- _backend/src/models/Notificator.js
+++ _backend/src/models/Notificator.js
@@ -3,7 +3,7 @@ let Notifica = require("./Notifica");
 class Notificator {
   creaNotifica(testo, visita, tipo) {
     const notifica = new Notifica(testo, visita, tipo);
-    return true;
+    return testo;
   }
 }
 
