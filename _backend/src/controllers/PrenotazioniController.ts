47ca4fc6a3a0aa54a691a2eded3110dd85b2afd1
Alessandra Boccuto <aless97b@gmail.com>
2019-01-05 15:43:22 +0100

    minor fix

--- _backend/src/controllers/PrenotazioniController.ts
+++ _backend/src/controllers/PrenotazioniController.ts
@@ -1,32 +1,35 @@
+import Tools from "../asset/tools";
 import Patient from "../models/Patient";
+import User from "../models/User";
 import Visita from "../models/Visita";
 import PatientController from "./PatientController";
+import VisitaManager from "./VisitaManager";
+
+const logger = Tools.Instance.getLogger("app:visite");
 
 export default class PrenotazioniController {
-  public static annullaPrenotazione(prenotazione: string): number | undefined {
+  public static annullaPrenotazione(prenotazione: string): void {
     try {
       const descrizionePrenotazione = JSON.parse(prenotazione);
-      const rp: number = descrizionePrenotazione._paziente._reputazione;
+      const p = descrizionePrenotazione._paziente;
       const visitaDaAnnullare = new Visita(
         descrizionePrenotazione._tipoVisita,
         descrizionePrenotazione._effettuata,
         descrizionePrenotazione._priorita,
         descrizionePrenotazione._pagata,
         new Date(descrizionePrenotazione._data),
-        new Patient(rp)
+        new Patient(new User(p._name, p._surname, p._address, p._birthdate))
       );
+
       const rep: number = PatientController.abbassaReputazione(
         visitaDaAnnullare.paziente,
         visitaDaAnnullare.data
       );
 
-      return rep;
+      VisitaManager.associaVisitaAnnullata(visitaDaAnnullare);
     } catch (e) {
-      console.log(e.message);
+      logger(e.message);
     }
-
-    // VisitaManager.Associa(x)
-
     // elimina paziente dalla prenotazione
   }
 }
