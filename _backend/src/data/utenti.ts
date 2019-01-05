9f6f202284155748ff401eefbb603414455a2b7a
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-01-05 15:39:24 +0100

    updated utenti

--- _backend/src/data/utenti.ts
+++ _backend/src/data/utenti.ts
@@ -1,8 +1,28 @@
 import User from "../models/User";
 const utenti = [
-  new User("Me", "Mario", "here", new Date(1997, 11, 27)),
-  new User("Me", "Luigi", "there", new Date(1999, 11, 27)),
-  new User("Me", "Wario", "here2", new Date(1999, 11, 15)),
-  new User("Me", "Waluigi", "there2", new Date(1999, 11, 15))
+  new User(
+    "Victor",
+    "Newman",
+    "7436 Schoolhouse St. Lumberton, NC 28358",
+    new Date(1997, 4, 7)
+  ),
+  new User(
+    "Cameron",
+    "Morrison",
+    "8004 Glenholme Ave. Owosso, MI 48867",
+    new Date(1999, 0, 5)
+  ),
+  new User(
+    "Emily",
+    "MacLeod",
+    "9171 Bradford Avenue Chapel Hill, NC 27516",
+    new Date(1999, 3, 17)
+  ),
+  new User(
+    "Carolyn",
+    "Mitchell",
+    "8364 Prince St. Bangor, ME 04401",
+    new Date(1999, 7, 15)
+  )
 ];
 export default utenti;
