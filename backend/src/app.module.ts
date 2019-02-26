0491a395cfce594abf2881611b6e4f717fad8c18
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-02-26 05:32:40 +0100

    removed unusefull code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrenotazioniModule } from './modules/prenotazioni/prenotazioni.module';
 import { StruttureModule } from './modules/strutture/strutture.module';
 import { RicettaModule } from './modules/ricetta/ricetta.module';
@@ -19,7 +17,5 @@ import { PatientModule } from './modules/patient/patient.module';
     VisitaModule,
     PatientModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
