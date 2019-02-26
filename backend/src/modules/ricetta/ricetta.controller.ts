20f1ed69782f895afad0b1a6dc9604fa62bc338e
Alessandro Giacche` <alessandrogiacch@gmail.com>
2019-02-26 04:14:00 +0100

    minor rename

--- backend/src/modules/ricetta/ricetta.controller.ts
+++ backend/src/modules/ricetta/ricetta.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Param, Render } from '@nestjs/common';
 import { RicettaService } from './ricetta.service';
 import { Types } from 'mongoose';
 
-@Controller('ricetta')
+@Controller('ricette')
 export class RicettaController {
   constructor(private readonly ricettaService: RicettaService) {}
 
